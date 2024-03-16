07e71077ced42d0e23be29f7aae895c266838ad2
Devansh Sharma <devsansh@yahoo.com>
2024-03-16 12:39:33 +0530

    Time period done

--- Project-1 { Simple Stock Price}/app.py
+++ Project-1 { Simple Stock Price}/app.py
@@ -7,10 +7,16 @@ st.title("Wall of Stocks:")
 # Date settings:
 st.header("Choose :blue[time_period]:red[:]")
 today = datetime.now()
+col1,col2 = st.columns(2)
+with col1:
+    st.subheader("Start Date:")
+    start_date = st.date_input(label = "Enter start date",value= today - timedelta (days=10*365),format="YYYY-MM-DD")
 
-start_date = st.date_input(label = "Enter start date",value= today - timedelta (days=10*365),format="YYYY-MM-DD")
+with col2:
+    st.subheader("End Date:")
+    end_date = st.date_input(label = "Enter end date",value="default_value_today",format="YYYY-MM-DD")
 
-end_date = st.date_input(label = "Enter end date",value="default_value_today",format="YYYY-MM-DD")
+
 all_stock_symbols = yf.Tickers('')
 
 tickerSymbol = "GOOGL"
