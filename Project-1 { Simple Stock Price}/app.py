b56acd819d10b32d20da4557bcab034808ee0bae
Devansh Sharma <devsansh@yahoo.com>
2024-03-16 12:30:30 +0530

    Simple Design done

---
+++ Project-1 { Simple Stock Price}/app.py
@@ -0,0 +1,20 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+from datetime import datetime, timedelta
+
+st.title("Wall of Stocks:")
+# Date settings:
+st.header("Choose :blue[time_period]:red[:]")
+today = datetime.now()
+
+start_date = st.date_input(label = "Enter start date",value= today - timedelta (days=10*365),format="YYYY-MM-DD")
+
+end_date = st.date_input(label = "Enter end date",value="default_value_today",format="YYYY-MM-DD")
+all_stock_symbols = yf.Tickers('')
+
+tickerSymbol = "GOOGL"
+tickerData = yf.Ticker(tickerSymbol)
+tickerDf = tickerData.history(period='id', start=start_date, end=end_date)
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
