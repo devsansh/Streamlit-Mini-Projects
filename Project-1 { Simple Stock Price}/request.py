c5afad1a2046f8f0b1ac4402b430fa3ea93407c3
Devansh Sharma <devsansh@yahoo.com>
2024-03-18 11:40:25 +0530

    Changes in code.

--- Project-1 { Simple Stock Price}/request.py
+++ Project-1 { Simple Stock Price}/request.py
@@ -1,17 +1,19 @@
-import config
 import streamlit as st
 import pandas as pd
 import requests
+import yfinance
 from datetime import datetime, timedelta
 
-def fetch_stock_data(symbol, start_date, end_date):
-  url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={API_KEY}&datatype=csv&startDate={start_date}&endDate={end_date}"
-  response = requests.get(url)
-  if response.status_code == 200:
-    return pd.read_csv(response.content)
-  else:
-    st.error(f"Error fetching data for {symbol}. Status code: {response.status_code}")
-    return None
+API_KEY = 'C6MLI6ZMEUDMGA3L'
+
+# def fetch_stock_data(symbol, start_date, end_date):
+#   url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={API_KEY}&datatype=csv&startDate={start_date}&endDate={end_date}"
+#   response = requests.get(url)
+#   if response.status_code == 200:
+#     return pd.read_csv(response.content)
+#   else:
+#     st.error(f"Error fetching data for {symbol}. Status code: {response.status_code}")
+#     return None
 
 st.title("Wall of Stocks:")
 
@@ -27,6 +29,7 @@ with col2:
   st.subheader("End Date:")
   today = datetime.now()  # Get the current date and time
   end_date = st.date_input(label="Enter end date", value=today) 
+
 # User input for stock symbol
 
 ###############
@@ -52,16 +55,3 @@ if df is not None:
     selected_row = df[df["symbol"] == selected_stock]
     stock_name = selected_row["name"].iloc[0]
     st.write(f"You selected: {stock_name} ({selected_stock})")
-
-
-###############
-tickerSymbol =st.button("Show Visuals","Primary")
-# Fetch data if symbol and dates are provided
-if tickerSymbol and start_date and end_date:
-  tickerDf = fetch_stock_data(tickerSymbol, start_date.strftime("%Y-%m-%d"), end_date.strftime("%Y-%m-%d"))
-  if tickerDf is not None:
-    # Assuming 'timestamp' is the date column and '4. close' is the closing price
-    st.line_chart(tickerDf[["timestamp", "4. close"]], x="timestamp", y="4. close", label="Closing Price")
-    # Assuming '5. volume' is the volume column
-    st.line_chart(tickerDf[["timestamp", "5. volume"]], x="timestamp", y="5. volume", label="Volume")
-
